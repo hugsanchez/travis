1fcb249b32a1cb8ea260b62aa2d7be0d4554d838
Hugo Sanchez <hugsanchez0917@gmail.com>
2021-04-22 20:31:04 -0400

    wrote first rout

--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,20 @@
 const { expect } = require('chai');
 
+const app = require('supertest')(require('../app'))
+
+
 describe('Testing', () => {
     it('equals 2', ()=>{
         expect(1 + 1).to.equal(2)
+    })
+});
+
+describe('Routes', () => {
+    describe('GET /', ()=>{
+        it('show info', async() =>{
+            const response = await app.get('/');
+            expect(response.status).to.equal(200);
+            expect(response.text).to.include('The Acme API');
+        })
     })
 })

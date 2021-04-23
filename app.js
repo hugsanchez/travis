8a7b4b313d4a93ed14ea9730c6afbc12d240f17c
Hugo Sanchez <hugsanchez0917@gmail.com>
2021-04-22 21:19:00 -0400

    created route for movies

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const { models: { Actor, Movie, Role}} = require('./db');
+
 
 app.get('/', (req,res) => {
     res.send(`
@@ -8,6 +10,11 @@ app.get('/', (req,res) => {
             <h1>Hello</h1>
         </html>
     `)
+})
+
+app.get('/api/movies', async(req,res,next) => {
+    const movies = await Movie.findAll();
+    res.send(movies);
 })
 
 module.exports = app;

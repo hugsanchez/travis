883d88bd433ff6872362c5d558db4e496fc4ff09
Hugo Sanchez <hugsanchez0917@gmail.com>
2021-04-27 22:39:45 -0400

    anotha one

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const { STRING } = Sequelize;
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db');
+const conn = new Sequelize(process.env.DATABASE_URL || "postgres://localhost/acme_db");
 
 const Movie = conn.define("movie", {
   title: STRING
@@ -22,25 +22,25 @@ Actor.hasMany(Role);
 const syncAndSeed = async()=> {
   await conn.sync({ force: true });
   const [sully, catchMeIfYouCan, truman, devil, hanks, linney, harris, leo, meryl] = await Promise.all([
-    Movie.create({ title: 'Sully' }),
-    Movie.create({ title: 'Catch Me If You Can' }),
-    Movie.create({ title: 'The Truman Show' }),
-    Movie.create({ title: 'The Devil Wears Prada' }),
-    Actor.create({ name: 'Tom Hanks' }),
-    Actor.create({ name: 'Laura Linney' }),
-    Actor.create({ name: 'Ed Harris' }),
-    Actor.create({ name: 'Leo' }),
-    Actor.create({ name: 'Meryl' }),
+    Movie.create({ title: "Sully" }),
+    Movie.create({ title: "Catch Me If You Can" }),
+    Movie.create({ title: "The Truman Show" }),
+    Movie.create({ title: "The Devil Wears Prada" }),
+    Actor.create({ name: "Tom Hanks" }),
+    Actor.create({ name: "Laura Linney" }),
+    Actor.create({ name: "Ed Harris" }),
+    Actor.create({ name: "Leo" }),
+    Actor.create({ name: "Meryl" }),
   ]);
   console.log(devil.get());
 
   await Promise.all([
-    Role.create({ character: 'Sully', movieId: sully.id, actorId: hanks.id}),
-    Role.create({ character: 'Carl Hanratty', movieId: catchMeIfYouCan.id, actorId: hanks.id}),
-    Role.create({ character: 'Loraine', movieId: sully.id, actorId: linney.id}),
-    Role.create({ character: 'Hannah Gill', movieId: truman.id, actorId: linney.id}),
-    Role.create({ character: 'Christoff', movieId: truman.id, actorId: harris.id}),
-    Role.create({ character: 'Miranda Presly', movieId: devil.id, actorId: meryl.id})
+    Role.create({ character: "Sully", movieId: sully.id, actorId: hanks.id}),
+    Role.create({ character: "Carl Hanratty", movieId: catchMeIfYouCan.id, actorId: hanks.id}),
+    Role.create({ character: "Loraine", movieId: sully.id, actorId: linney.id}),
+    Role.create({ character: "Hannah Gill", movieId: truman.id, actorId: linney.id}),
+    Role.create({ character: "Christoff", movieId: truman.id, actorId: harris.id}),
+    Role.create({ character: "Miranda Presly", movieId: devil.id, actorId: meryl.id})
   ]);
 };
 
